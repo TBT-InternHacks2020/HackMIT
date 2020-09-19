7f4dc5748a1d9a464f28165b4797aa81a3221ac7
tgaylord <tgaylord2024@gmail.com>
2020-09-19 12:41:23 -0400

    Firebase Config

---
+++ database.js
@@ -0,0 +1,13 @@
+  var firebaseConfig = {
+        apiKey: "AIzaSyCFPzZwAMi9R3ZCRMhcB3pf4Ak1Bo6x19g",
+        authDomain: "hackmit-d84b7.firebaseapp.com",
+        databaseURL: "https://hackmit-d84b7.firebaseio.com",
+        projectId: "hackmit-d84b7",
+        storageBucket: "hackmit-d84b7.appspot.com",
+        messagingSenderId: "163971061339",
+        appId: "1:163971061339:web:53a7d6a5e3d20404f8517e",
+        measurementId: "G-TBT1HJCNYL"
+      };
+      // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
