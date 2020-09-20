8f6f7d1e5276f5259769cb89ee881f7d65d26ea1
HannahGuo <hguo2102@gmail.com>
2020-09-19 20:29:02 -0400

    sticky align

--- public/scripts/stickyNotes.js
+++ public/scripts/stickyNotes.js
@@ -7,60 +7,39 @@ class StickyNote {
     }
 }
 
-let currentStickyNotes = [new StickyNote("Robotics", "September 4th", "Robots!!!", "Second General Meeting"), new StickyNote("Robotics", "September 4th", "Robots!!!", "First General Meeting"), new StickyNote("Robotics", "September 4th", "Robots!!!", "First General Meeting"), new StickyNote("Robotics", "September 4th", "Robots!!!", "Third General Meeting")];
+let currentStickyNotes = [new StickyNote("Robotics", "September 4th", "Robots!!!", "Second General Meeting"), new StickyNote("Robotics", "September 4th", "Robots!!!", "Second General Meeting"), new StickyNote("Robotics", "September 4th", "Robots!!!", "First General Meeting"), new StickyNote("Robotics", "September 4th", "Robots!!!", "First General Meeting"), new StickyNote("Robotics", "September 4th", "Robots!!!", "Third General Meeting")];
 
 function addStickyNotesToRow() {
-    let stickiesPerRow = 3;
+    let stickiesPerRow = 5;
+    let stringToAdd1 = "";
+
     for (let i = 0; i < Math.floor(currentStickyNotes.length / stickiesPerRow); i++) {
-        $('#sports').append(`<tr>
-        <td>
-        <div class="stickyNote">
-        <h3>${currentStickyNotes[i].eventName}</h3>
-        <p>${currentStickyNotes[i].eventDescription}</p>
-        </div>
-        </td>
-        <td>
-        <div class="stickyNote">
-        <h3>${currentStickyNotes[i + 1].eventName}</h3>
-        <p>${currentStickyNotes[i + 1].eventDescription}</p>
-        </div>
-        </td>
-        <td>
-        <div class="stickyNote">
-        <h3>${currentStickyNotes[i + 2].eventName}</h3>
-        <p>${currentStickyNotes[i + 2].eventDescription}</p>
-        </div>
-        </td>
-        </tr>`);
+        for(let j = 0; j < stickiesPerRow; j++) {
+            stringToAdd1 += `<td>
+            <div class="stickyNote">
+            <h3>${currentStickyNotes[j].eventName}</h3>
+            <p>${currentStickyNotes[j].eventDescription}</p>
+            </div>
+            </td>`
+        }
+        $('#sports').append("<tr>" + stringToAdd1 + "</tr>");
+        stringToAdd1 = "";
     }
 
-    if (currentStickyNotes.length % stickiesPerRow != 0) {
-        if (currentStickyNotes.length % stickiesPerRow == 1) {
-            $('#sports').append(`<tr>
-            <td>
-            <div class="stickyNote">
-            <h3>${currentStickyNotes[currentStickyNotes.length - 1].eventName}</h3>
-            <p>${currentStickyNotes[currentStickyNotes.length - 1].eventDescription}</p>
-            </div>
-            </td>
-            </tr>`);
-        } else if (currentStickyNotes.length % stickiesPerRow == 2) {
-            $('#sports').append(`<tr>
-            <td>
-            <div class="stickyNote">
-            <h3>${currentStickyNotes[currentStickyNotes.length - 1].eventName}</h3>
-            <p>${currentStickyNotes[currentStickyNotes.length - 1].eventDescription}</p>
-            </div>
-            </td>
-            <td>
-            <div class="stickyNote">
-            <h3>${currentStickyNotes[currentStickyNotes.length - 2].eventName}</h3>
-            <p>${currentStickyNotes[currentStickyNotes.length - 2].eventDescription}</p>
-            </div>
-            </td>
-            </tr>`);
-        }
+    let stringToAdd2 = "hello";
+
+    for (let i = 0; i < currentStickyNotes.length % stickiesPerRow; i++) {
+        stringToAdd2 += `
+        <td>
+        <div class="stickyNote">
+        <h3>${currentStickyNotes[currentStickyNotes.length - (i + 1)].eventName}</h3>
+        <p>${currentStickyNotes[currentStickyNotes.length - (i + 1)].eventDescription}</p>
+        </div>
+        </td>
+        `;
     }
+
+    $('#sports').append("<tr>" + stringToAdd2 + "</tr>");
 }
 
 addStickyNotesToRow();
