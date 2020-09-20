47bcad1522bdbc3961bfc669fba40e30032b8ba2
Maniya Dahiya <51750774+md2002@users.noreply.github.com>
2020-09-20 16:42:53 +0530

    read info class

--- public/scripts/read.js
+++ public/scripts/read.js
@@ -1,43 +1,24 @@
-var database = firebase.database();
 
-function readData() {
+class readinfo {
+    var database = firebase.database();
+
     database.ref().on("value", function (snapshot) {
-        var numOrgs = snapshot.numChildren();
-        snapshot.forEach((childSnapshot) => {
-            var snap = childSnapshot.val();
-            var Club_email = snap["Club_email"];
+    var numOrgs = snapshot.numChildren();
+    snapshot.forEach((childSnapshot) => {
+    var snap = childSnapshot.val();
+    constructor(snap["Club_email"], snap["Club_name"], snap["Description"], snap["Event_Title"], snap["Post_Date"], snap["Sign_Up_Link"], snap["Social_Link"], snap["Tag1"], snap["Tag2"], snap["Tag3"]){
 
-            if (snap && snap["Club_name"])
-                var Club_name = snap["Club_name"];
+        this.Club_email = snap["Club_email"]
+        this.Club_name = snap["Club_name"]
+        this.Description = snap["Description"]
+        this.Event_Title = snap["Event_Title"]
+        this.Post_Date = snap["Post_Date"]
+        this.Tage1 = snap["Tag1"]
+        this.Tag2 = snap["Tag2"]
+        this.Tag3 = snap["Tag3"]
+        this.Sign_Up_Link = snap["Sign_Up_Link"]
+        this.Social_Link = snap["Social_Link"]
 
-            if (snap && snap["Description"])
-                var Description = snap["Description"];
+    }
 
-            if (snap && snap["Event_Title"])
-                var Event_Title = snap["Event_Title"];
-
-            if (snap && snap["Post_Date"])
-                var Post_Date = snap["Post_Date"];
-
-            if (snap && snap["Club_email"])
-                var latb = snap["Club_email"];
-
-            if (snap && snap["Sign_Up_Link"])
-                var Sign_Up_Link = snap["Sign_Up_Link"];
-
-            if (snap && snap["Social_Link"])
-                var Social_Link = snap["Social_Link"];
-
-            if (snap && snap["Tag1"])
-                var Tag1 = snap["Tag1"];
-
-            if (snap && snap["Tag2"])
-                var Tag2 = snap["Tag2"];
-
-            if (snap && snap["Tag3"])
-                var Tag3 = snap["Tag3"];
-        });
-    });
 }
-
-readData();
